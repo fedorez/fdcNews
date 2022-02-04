b411b099f960d81f8111c103654ff9b4e41745a9
fedorez <fedorez@mail.ru>
2022-02-04 18:56:24 +0300

    first

---
+++ fdcNews/fdcNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fdcNews
+//
+//  Created by Denis Fedorets on 04.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
